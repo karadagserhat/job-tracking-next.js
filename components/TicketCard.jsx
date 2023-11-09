0fcf636176a0669ab4c15312d7f63edec7de92dc
karadagserhat <serhatkaradagg@outlook.com>
2023-11-09 10:16:53 +0300

    first commit

--- components/TicketCard.jsx
+++ components/TicketCard.jsx
@@ -1,9 +1,29 @@
 import DeleteBlock from './DeleteBlock';
+import PriorityDisplay from './PriorityDisplay';
+import StatusDisplay from './StatusDisplay';
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
+    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Job Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">Lorem ipsum dolor sit amet.</p>
+
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex ">
+          <p className="text-xs mt-auto">06/15-1995</p>
+        </div>
+        <div className="ml-auto  flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
